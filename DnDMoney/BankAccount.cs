9f7283d8e8b186923424a3b190730057b763ddf3
Logan Grisham <logangrisham@yahoo.com>
2022-08-20 20:17:38 -0500

    Created Bank accounts

---
+++ DnDMoney/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDMoney
+{
+    [Serializable]
+    public class BankAccount
+    {
+        public int accountId { get; set; }
+        public string partyName { get; set; }
+        public double balance { get; set; }
+
+    }
+}
